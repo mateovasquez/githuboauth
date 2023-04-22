e0c83afdda0cbc05fc2da23bcf74ad1201df02b8
Mateo <mateo.vasquezco@gmail.com>
2023-04-22 20:42:55 +0200

    refactor tests

--- githuboauth/tests/test_urls.py
+++ githuboauth/tests/test_urls.py
@@ -7,32 +7,23 @@ class TestLoginRequired:
   home_url = reverse("home")
   profile_url = reverse("my_profile")
 
-  def test_home_access_logged_in(self, client):
+  def test_pages_access_logged_in(self, client):
     user = User.objects.create()
     client.force_login(user=user)
-    response_content = client.get(self.home_url).content
 
-    assert b'You are logged in' in response_content
+    response_home = client.get(self.home_url).content
+    assert b'You are logged in' in response_home
+
+    response_profile = client.get(self.profile_url).content
+    assert b'This is my profile' in response_profile
 
     user.delete()
 
-  def test_home_access_logged_out(self, client):
-    response = client.get(self.home_url)
+  def test_pages_access_logged_out(self, client):
     redirect_status_code = 302
 
-    assert response.status_code == redirect_status_code
+    response_home = client.get(self.home_url)
+    assert response_home.status_code == redirect_status_code
 
-  def test_profile_access_logged_in(self, client):
-    user = User.objects.create()
-    client.force_login(user=user)
-    response_content = client.get(self.profile_url).content
-
-    assert b'This is my profile' in response_content
-
-    user.delete()
-
-  def test_profile_access_logged_out(self, client):
-    response = client.get(self.profile_url)
-    redirect_status_code = 302
-    
-    assert response.status_code == redirect_status_code
+    response_profile = client.get(self.home_url)
+    assert response_profile.status_code == redirect_status_code
