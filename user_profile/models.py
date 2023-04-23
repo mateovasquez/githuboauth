6e4804be5a27c9c06f5680485a69c81e61f34529
Mateo <mateo.vasquezco@gmail.com>
2023-04-23 12:43:42 +0200

    adapted profile model null=true

--- user_profile/models.py
+++ user_profile/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Profile(models.Model):
-  user = models.OneToOneField(User, on_delete=models.CASCADE)
+  user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
   phone_number = models.CharField(max_length=9, blank=True)
   location = models.CharField(max_length=100, blank=True)
   address = models.CharField(max_length=500, blank=True)
