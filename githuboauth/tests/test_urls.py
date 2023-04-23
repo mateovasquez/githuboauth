a9d5ab41bff0501691f76a3cc9c9f0edd36621fe
Mateo <mateo.vasquezco@gmail.com>
2023-04-23 16:02:46 +0200

    adapted urls test

--- githuboauth/tests/test_urls.py
+++ githuboauth/tests/test_urls.py
@@ -4,8 +4,8 @@ from django.contrib.auth.models import User
 
 @pytest.mark.django_db
 class TestLoginRequired:
-  home_url = reverse("home")
-  profile_url = reverse("user_detail")
+  home_url = reverse('home')
+  user_url = reverse('user_detail')
 
   def test_pages_access_logged_in(self, client):
     user = User.objects.create()
@@ -14,7 +14,7 @@ class TestLoginRequired:
     response_home = client.get(self.home_url).content
     assert b'You are logged in' in response_home
 
-    response_profile = client.get(self.profile_url).content
+    response_profile = client.get(self.user_url).content
     assert b'User Settings' in response_profile
 
     user.delete()
@@ -22,33 +22,13 @@ class TestLoginRequired:
   def test_pages_access_logged_out(self, client):
     redirect_status_code = 302
     login_redirect_home_url = '/accounts/login/?next=/'
-    login_redirect_profile_url = '/accounts/login/?next=/user'
+    login_redirect_user_url = '/accounts/login/?next=/user'
 
     response_home = client.get(self.home_url)
     assert response_home.url == login_redirect_home_url
     assert response_home.status_code == redirect_status_code
 
-    response_profile = client.get(self.profile_url)
+    response_profile = client.get(self.user_url)
 
-    assert response_profile.url == login_redirect_profile_url
+    assert response_profile.url == login_redirect_user_url
     assert response_profile.status_code == redirect_status_code
-
-@pytest.mark.django_db
-class TestGetUserInfo:
-  profile_url = reverse("user_detail")
-
-  def test_user_info(self, client):
-    user = User.objects.create(
-      username="wolf&badger",
-      first_name="wolf",
-      last_name="badger"
-    )
-    client.force_login(user=user)
-    response = client.get(self.profile_url)
-
-    response_data = response.context['user_form'].__dict__['initial']
-    assert response_data['username'] == user.username
-    assert response_data['first_name'] == user.first_name
-    assert response_data['last_name'] == user.last_name
-
-    user.delete()
