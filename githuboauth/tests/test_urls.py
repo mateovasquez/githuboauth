7baad50c7901f42918b381cd8b10283dabfdbf2c
Mateo <mateo.vasquezco@gmail.com>
2023-04-23 08:44:29 +0200

    add test get user info

--- githuboauth/tests/test_urls.py
+++ githuboauth/tests/test_urls.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 import pytest
 from django.urls import reverse
 from django.contrib.auth.models import User
@@ -21,9 +22,34 @@ class TestLoginRequired:
 
   def test_pages_access_logged_out(self, client):
     redirect_status_code = 302
+    login_redirect_home_url = '/accounts/login/?next=/'
+    login_redirect_profile_url = '/accounts/login/?next=/my-profile'
 
     response_home = client.get(self.home_url)
+    assert response_home.url == login_redirect_home_url
     assert response_home.status_code == redirect_status_code
 
-    response_profile = client.get(self.home_url)
+    response_profile = client.get(self.profile_url)
+
+    assert response_profile.url == login_redirect_profile_url
     assert response_profile.status_code == redirect_status_code
+
+@pytest.mark.django_db
+class TestGetUserInfo:
+  profile_url = reverse("my_profile")
+
+  def test_user_info(self, client):
+    user = User.objects.create(
+      username="wolf&badger",
+      first_name="wolf",
+      last_name="badger"
+    )
+    client.force_login(user=user)
+    response = client.get(self.profile_url)
+
+    response_data = response.context['user_form'].__dict__['initial']
+    assert response_data['username'] == user.username
+    assert response_data['first_name'] == user.first_name
+    assert response_data['last_name'] == user.last_name
+
+    user.delete()
